a56d7841e2e16378e3ad18f88a423f4eca9ac02e
mahome_309C_ubuntu <wintowanti@163.com>
2016-12-21 22:14:46 +0800

    add next max_id

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 _author_ = "yanyao"
 
 from twitter import *
+import re
 
 my_auth = OAuth(token="719348056827633664-sMMJNecL1IzAYiPG4SFUqTVmqW2aMuf",
                 token_secret="5WwPmSvFKbzroWeMLPBKhCyw8JFJY27lrwkiVmihFHig4",
@@ -9,16 +10,30 @@ my_auth = OAuth(token="719348056827633664-sMMJNecL1IzAYiPG4SFUqTVmqW2aMuf",
                 consumer_secret="YqxvHw9j4KObanExFptFTCjBk4jrZFCtOAzYPl3BZXgVvdSX0Q"
                 )
 t = Twitter(auth=my_auth)
-max_id = None
-for _ in range(10):
-    if max_id is None:
-        ans = t.search.tweets(q="#atheism", count=100)
-    else:
-        ans = t.search.tweets(q="#atheism", count=100, max_id=max_id)
-    max_id = ans[u"search_metadata"][u"max_id"]
-    for item in ans[u"statuses"]:
-        print "\n-------------------------------------------"
-        print item[u"text"].strip().encode("utf-8")
-        print "-------------------------------------------\n"
 
+def main():
+    max_id = None
+    for _ in range(10):
+        if max_id is None:
+            ans = t.search.tweets(q="#atheism", count=100)
+        else:
+            ans = t.search.tweets(q="#atheism", count=100, max_id=max_id)
+        max_id = get_next_max_id(ans[u"search_metadata"][u"next_results"])
+        for item in ans[u"statuses"]:
+            print "\n-------------------------------------------"
+            print item[u"text"].strip().encode("utf-8")
+            print "-------------------------------------------\n"
+
+
+def get_next_max_id(next_query_str):
+    pattern = re.compile(u"\?max_id=(\d*)")
+    match = pattern.match(next_query_str)
+    if match is not None:
+        return int(match.group(1))
+    else:
+        return 0
+if __name__ == "__main__":
+    #t1 = get_next_max_id(u'?max_id=811542643846975487&q=%23atheism&count=100&include_entities=1')
+    #pass
+    main()
 
